8190c9745e26b67a7d062a17e4e1078a453e8d27
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-14 11:40:16 +0900

    Fix.

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ "$TESTING_FRAMEWORK" = "PHPUnit" ]; then
-  phpunit
+  phpunit --coverage-text
 elif [ "$TESTING_FRAMEWORK" = "Behat" ]; then
   wget https://github.com/downloads/Behat/Behat/behat.phar
   php behat.phar
