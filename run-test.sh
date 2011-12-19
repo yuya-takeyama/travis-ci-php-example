4febb4747076a8b6aa4af952e17cb81c989743ed
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-19 21:07:43 +0900

    Runs PHPUnit and Behat

--- run-test.sh
+++ run-test.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-wget https://github.com/downloads/Behat/Behat/behat.phar
-php behat.phar
+if [ "$TESTING_FRAMEWORK" = "PHPUnit" ]; then
+  phpunit
+elif [ "$TESTING_FRAMEWORK" = "Behat" ]; then
+  wget https://github.com/downloads/Behat/Behat/behat.phar
+  php behat.phar
+fi
+
+exit $?
