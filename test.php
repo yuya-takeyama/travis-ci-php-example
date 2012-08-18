3860ca593eae552eb4dbefd37a507cd1c49b0e8f
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-19 05:48:45 +0900

    Add proc_open

--- test.php
+++ test.php
@@ -3,3 +3,13 @@ echo '$_ENV:', PHP_EOL;
 var_dump($_ENV);
 echo '$_SERVER:', PHP_EOL;
 var_dump($_SERVER);
+
+$fdSpecs = array(
+    array('pipe', 'r'),
+    array('pipe', 'w'),
+    array('pipe', 'w'),
+);
+$process = proc_open('php test2.php', $fdSpecs, $pipes, null, array('foo' => 'bar'));
+
+echo stream_get_contents($pipes[1]);
+echo stream_get_contents($pipes[2]);
