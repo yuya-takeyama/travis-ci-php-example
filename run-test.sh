a36a736bb3f6955a39355a664316fe0dbe6edb5b
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-25 12:03:35 +0900

    test

--- run-test.sh
+++ run-test.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-if [ "$TESTING_FRAMEWORK" = "PHPUnit" ]; then
-  phpunit --coverage-text
-elif [ "$TESTING_FRAMEWORK" = "Behat" ]; then
-  wget https://github.com/downloads/Behat/Behat/behat.phar
-  php behat.phar
-fi
-
+php test.php
 exit $?
--- test.php
+++ test.php
@@ -1,15 +1,16 @@
 <?php
-echo '$_ENV:', PHP_EOL;
-var_dump($_ENV);
-echo '$_SERVER:', PHP_EOL;
-var_dump($_SERVER);
-
 $fdSpecs = array(
     array('pipe', 'r'),
     array('pipe', 'w'),
     array('pipe', 'w'),
 );
-$process = proc_open('php test2.php', $fdSpecs, $pipes, null, array('foo' => 'bar'));
-
-echo stream_get_contents($pipes[1]);
-echo stream_get_contents($pipes[2]);
+$process = proc_open('php -r "exit;"', $fdSpecs, $pipes);
+var_dump(proc_get_status($process));
+echo "----------", PHP_EOL;
+echo "TERMINATE", PHP_EOL;
+echo "----------", PHP_EOL;
+foreach (range(1, 10) as $i) {
+    var_dump(proc_get_status($process));
+    echo "----------", PHP_EOL;
+    sleep(1);
+}
